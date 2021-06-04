1d7daf6489eb3e3f00bb50b2a1d25fe3362f94ae
Tim Home <a0963573232@gmail.com>
2021-06-04 18:09:55 +0800

    Fixed: 將分頁獨立為元件

---
+++ src/components/Navigate.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navigate">
+    <router-link
+      class="navigate-item"
+      :to="{ path: '/Page/', name: 'Home', params: { id: 1 } }"
+    >
+      <i class="fas fa-angle-double-left"></i>
+    </router-link>
+    <router-link
+      :class="['navigate-item', { active: page === num }]"
+      v-for="num in pages"
+      :key="num"
+      :to="{ path: '/Page/', name: 'Home', params: { id: num } }"
+    >
+      {{ num }}
+    </router-link>
+    <router-link
+      class="navigate-item"
+      :to="{ path: '/Page/', name: 'Home', params: { id: pages } }"
+    >
+      <i class="fas fa-angle-double-right"></i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Navigate",
+  props: {
+    pages: Number,
+    page: Number,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,27 +29,24 @@
     </div>
     <ActItemLoading v-if="!dataReady" />
     <article v-else>
+      <Navigate :pages="Math.ceil(showAct.length / 5)" :page="page" />
       <ActItem v-for="act in myShowAct" :key="act.actId" :act="act" />
-      <span v-for="num in Math.ceil(showAct.length / 5)" :key="num">
-        <router-link
-          :to="{ path: '/Page/', name: 'Home', params: { id: num } }"
-        >
-          {{ num }}
-        </router-link>
-      </span>
+      <Navigate :pages="Math.ceil(showAct.length / 5)" :page="page" />
     </article>
   </div>
 </template>
 
 <script>
-import ActItem from "@/components/ActItem.vue";
-import ActItemLoading from "@/components/ActItemLoading.vue";
 import { computed, reactive } from "vue";
 import { useRoute } from "vue-router";
+import Navigate from "@/components/Navigate.vue";
+import ActItem from "@/components/ActItem.vue";
+import ActItemLoading from "@/components/ActItemLoading.vue";
 
 export default {
   name: "Home",
   components: {
+    Navigate,
     ActItem,
     ActItemLoading,
   },
@@ -69,65 +66,7 @@ export default {
         page.value * props.perPage
       )
     );
-    console.log(myShowAct);
     return { myFilter, page, myShowAct };
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import "@/assets/scss/_variables.scss";
-
-.home {
-  height: calc(100vh - 80px);
-  padding: 1.5rem;
-  box-sizing: border-box;
-  overflow-y: scroll;
-  @include mobile {
-    height: auto;
-    padding: 1.5rem 0;
-  }
-}
-.overview {
-  font-size: 1.5rem;
-  font-weight: bold;
-  &-num {
-    color: $c_info;
-    font-size: 2rem;
-  }
-}
-.tags {
-  display: flex;
-  flex-wrap: wrap;
-  &-item {
-    position: relative;
-    margin: 0.5rem;
-    padding: 0.5rem 2.5rem 0.5rem 1.5rem;
-    color: $c_info;
-    border: 1px solid $c_info;
-    border-radius: 1rem;
-    transition: color 0.5s, background-color 0.5s;
-    cursor: pointer;
-    &:hover {
-      color: $c_light;
-      background-color: $c_info;
-    }
-  }
-  &-icon {
-    position: absolute;
-    top: 50%;
-    right: 0.5rem;
-    transform: translateY(-50%);
-  }
-  &-checkbox {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    cursor: pointer;
-    opacity: 0;
-  }
-}
-</style>
